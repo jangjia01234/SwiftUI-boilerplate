4ce358c83fa4c1c7c80e1c2b63c3a1b9a69732f0
Sangyun Jeong <jsangyun1039@naver.com>
2024-07-29 15:11:55 +0900

    Feat: 프로젝트 생성

---
+++ PiPi/PiPi/PiPiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiPiApp.swift
+//  PiPi
+//
+//  Created by 정상윤 on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PiPiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
