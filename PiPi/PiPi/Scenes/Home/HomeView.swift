0db966590fec7e314fa039181d1ff2a7de4efc37
Sangyun Jeong <jsangyun1039@naver.com>
2024-07-30 14:49:27 +0900

    Add: HomeView 파일 생성
    - Home 그룹 생성
    - HomeView.swift 파일 생성
    - ContentView에 HomeView 추가

---
+++ PiPi/PiPi/Scenes/Home/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  PiPi
+//
+//  Created by 정상윤 on 7/30/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+    
+}
+
+#Preview {
+    HomeView()
+}
