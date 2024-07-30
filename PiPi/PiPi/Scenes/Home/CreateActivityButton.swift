8ba64cf9f4db26ab80920e6b1d306ac71e765050
Sangyun Jeong <jsangyun1039@naver.com>
2024-07-31 08:44:22 +0900

    Feat: CreateActivityButton 구현
    - 플로팅 버튼 구현

---
+++ PiPi/PiPi/Scenes/Home/CreateActivityButton.swift
@@ -0,0 +1,30 @@
+//
+//  CreateActivityButton.swift
+//  PiPi
+//
+//  Created by 정상윤 on 7/31/24.
+//
+
+import SwiftUI
+
+struct CreateActivityButton: View {
+    
+    var body: some View {
+        Button(action: {
+            // TODO: 활동 등록 화면 시트 표시
+        }) {
+            Image(systemName: "plus")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .bold()
+        }
+        .frame(width: 60, height: 60)
+        .background(.white)
+        .clipShape(Circle())
+    }
+    
+}
+
+#Preview {
+    CreateActivityButton()
+}
