db778a40029a34365758433312c3aa6e390e791a
Sangyun Jeong <jsangyun1039@naver.com>
2024-07-31 09:54:01 +0900

    Feat: 홈 화면 UI 구현

--- PiPi/PiPi/Scenes/Home/HomeView.swift
+++ PiPi/PiPi/Scenes/Home/HomeView.swift
@@ -10,20 +10,57 @@ import MapKit
 
 struct HomeView: View {
     
+    @Namespace private var mapScope
+    
     var body: some View {
         ZStack {
-            VStack {
-                CategoryFilterView()
-                Spacer()
+            Map(scope: mapScope) {
+                
+            }
+            .mapControlVisibility(.hidden)
+            .zIndex(1)
+            
+            ZStack {
+                VStack {
+                    CategoryFilterView()
+                    TicketProfileButtonView()
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    VStack(spacing: 10) {
+                        Spacer()
+                        VStack(spacing: 5) {
+                            Button(action: {
+                                // TODO: 새로고침 동작 구현
+                            }) {
+                                Image(systemName: "arrow.triangle.2.circlepath")
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                            .setSmallButtonAppearance()
+                            MapUserLocationButton(scope: mapScope)
+                                .setSmallButtonAppearance()
+                        }
+                        ActivityCreateButton()
+                    }
+                }
             }
             .padding(.horizontal)
             .zIndex(2)
-            
-            Map {
-                
-            }
-            .zIndex(1)
         }
+        .mapScope(mapScope)
+    }
+    
+}
+
+fileprivate extension View {
+    
+    func setSmallButtonAppearance() -> some View {
+        self
+            .frame(width: 38, height: 38)
+            .background(.white)
+            .clipShape(Circle())
+            .setShadow()
     }
     
 }
