ad927e631c7cb1692c59f607dc43c1f69d0a4fc7
hyeyeonie <shy020916@naver.com>
2024-07-31 16:30:35 +0900

    [Feat]: 온보딩 구현 및 프로필 설정 페이지 구현
    - 온보딩 사진 위치 고정되도록 조정
    - 온보딩 수정, 화면 분할
    - 건너뛰기 버튼 생성
    - 화면이 전환되었을 때 pageindex 없애기
    
    - 프로필뷰 마지막 수정(iCloud 메일 고정)

--- PiPi/PiPi/Scenes/Onboarding/OnboardingLastPageView.swift
+++ PiPi/PiPi/Scenes/Onboarding/OnboardingLastPageView.swift
@@ -2,94 +2,67 @@
 //  OnboardingLastPageView.swift
 //  PiPi
 //
-//  Created by 신혜연 on 7/30/24.
+//  Created by 신혜연 on 7/31/24.
 //
 
 import SwiftUI
 
 struct OnboardingLastPageView: View {
-
-    @Binding var isFirstLaunching: Bool
-    @State private var nickname: String = ""
-    @State private var affiliation: String = ""
-    @State private var address: String = ""
     
-    let affiliations = [
-        "포항공대",
-        "애플 디벨로퍼 아카데미"
-    ]
+    @State private var isFirstLaunching = true
+    let imageName: String
+    let title: String
+    let subtitle: String
+    @Binding var navigateToProfile: Bool
     
     var body: some View {
-        VStack {
-            Text("프로필을\n 설정해주세요!")
-                .multilineTextAlignment(.center)
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.vertical, 64)
-            
-            VStack(spacing: 6) {
-                Text("닉네임")
-                    .font(.subheadline)
-                    .padding(.trailing, 280)
-                
-                TextField("닉네임을 입력해주세요.", text: $nickname)
-                    .textFieldStyle(PlainTextFieldStyle())
-                    .padding(.vertical, 12)
-                    .padding(.horizontal, 20)
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(8)
-                
-                Text("소속")
-                    .font(.subheadline)
-                    .padding(.trailing, 280)
-                
-                Picker("소속을 선택해주세요.", selection: $affiliation) {
-                    ForEach(affiliations, id: \.self) { affiliation in
-                        Text(affiliation).tag(affiliation)
-                    }
+        NavigationStack {
+            ZStack {
+                VStack {
+                    Spacer()
+                    Image(imageName)
+                        .resizable()
+                        .frame(width: 288, height: 290)
+                        .padding(.bottom, 36)
+                    Text(title)
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 26)
+                    Text(subtitle)
+                        .multilineTextAlignment(.center)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                    Spacer()
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(.vertical, 6)
-                .padding(.horizontal, 20)
-                .background(Color(UIColor.systemGray6))
-                .cornerRadius(8)
+                .padding(.bottom, 20)
                 
-                Text("이메일")
-                    .font(.subheadline)
-                    .padding(.trailing, 280)
-                
-                TextField("iCloud 이메일을 입력해주세요.", text: $address)
-                    .textFieldStyle(PlainTextFieldStyle())
-                    .autocapitalization(.none) // 첫 대문자 비활성화
-                    .padding(.vertical, 12)
-                    .padding(.horizontal, 20)
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(8)
+                Button {
+                    //버튼을 누르면 OnboardingProfileView로 이동
+                    navigateToProfile = true
+                } label: {
+                    Text("시작하기")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(width: 332, height: 48)
+                        .background(Color.purple)
+                        .cornerRadius(10)
+                }
+                .padding(.top, 600)
+                Button {
+                    //버튼을 누르면 OnboardingProfileView로 이동
+                    navigateToProfile = true
+                } label: {
+                    Text("건너뛰기")
+                        .foregroundColor(Color.purple)
+                        .font(.system(size: 16))
+                }
+                .padding(.bottom, 720)
+                .padding(.leading, 280)
             }
-            .padding(.horizontal, 36)
-            .padding(.bottom, 25)
-            
-            Button {
-                isFirstLaunching.toggle()
-            } label: {
-                Text("시작하기")
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .frame(width: 332, height: 48)
-                    .background(Color.purple)
-                    .cornerRadius(10)
+            .navigationDestination(isPresented: $navigateToProfile) {
+                OnboardingProfileView(isFirstLaunching: $isFirstLaunching)
             }
-            .padding(.bottom, 186)
         }
-    }
-}
-
-struct OnboardingLastPageView_Previews: PreviewProvider {
-    @State static var isFirstLaunching = true
-    
-    static var previews: some View {
-        OnboardingLastPageView(isFirstLaunching: $isFirstLaunching)
-            .previewLayout(.sizeThatFits)
-            .padding()
     }
 }
