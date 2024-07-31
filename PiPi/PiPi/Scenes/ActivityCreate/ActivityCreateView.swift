85573ab140469cbd1ac202b56e0ae25ea57a0a9d
Sangyun Jeong <jsangyun1039@naver.com>
2024-07-31 11:39:50 +0900

    Add: ActivityCreateView 파일 생성
    - ActivityCreate 그룹 생성
    - ActivityCreateView.swift 파일 생성

---
+++ PiPi/PiPi/Scenes/ActivityCreate/ActivityCreateView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityCreateView.swift
+//  PiPi
+//
+//  Created by 정상윤 on 7/31/24.
+//
+
+import SwiftUI
+
+struct ActivityCreateView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ActivityCreateView()
+}
