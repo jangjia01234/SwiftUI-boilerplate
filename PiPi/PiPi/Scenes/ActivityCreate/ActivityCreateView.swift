216bb77ededb818d69056758c91d2237708d5055
Sangyun Jeong <jsangyun1039@naver.com>
2024-08-01 17:32:24 +0900

    Refactor: ActivityInformationFormView 적용

--- PiPi/PiPi/Scenes/ActivityCreate/ActivityCreateView.swift
+++ PiPi/PiPi/Scenes/ActivityCreate/ActivityCreateView.swift
@@ -22,58 +22,15 @@ struct ActivityCreateView: View {
     
     var body: some View {
         NavigationStack {
-            Form {
-                Section {
-                    TextField("", text: $title)
-                        .focused($isFocused)
-                } header: {
-                    header(title: "제목", subtitle: nil)
-                }
-                
-                Section {
-                    TextEditor(text: $description)
-                        .focused($isFocused)
-                } header: {
-                    header(title: "설명", subtitle: nil)
-                }
-                Section {
-                    Stepper("\(maxPeopleNumber)", value: $maxPeopleNumber, in: 1...20)
-                } header: {
-                    header(title: "최대인원", subtitle: "20명까지 가능합니다.")
-                }
-                
-                Picker(selection: $category) {
-                    ForEach(Activity.Category.allCases, id: \.self) { category in
-                        Text(category.rawValue).tag(category as Activity.Category?)
-                    }
-                } label: {
-                    Text("카테고리")
-                }
-                
-                Section {
-                    DatePicker("시작 일시", selection: $startDateTime, in: Date()...)
-                    Picker(selection: $estimatedTime) {
-                        Text("미정").tag(nil as Int?)
-                        ForEach(1..<6) { time in
-                            Text("\(time)").tag(time as Int?)
-                        }
-                    } label: {
-                        Text("예상 소요시간")
-                    }
-                    NavigationLink(destination: {
-                        LocationSelectView(coordinates: $location)
-                            .navigationBarBackButtonHidden()
-                    }) {
-                        HStack {
-                            Text("위치")
-                            Text(location == nil ? "" : "선택 완료")
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                                .font(.caption)
-                                .foregroundStyle(.secondary)
-                        }
-                    }
-                }
-            }
+            ActivityInformationFormView(
+                title: $title,
+                description: $description,
+                maxPeopleNumber: $maxPeopleNumber,
+                category: $category,
+                startDateTime: $startDateTime,
+                estimatedTime: $estimatedTime,
+                location: $location
+            )
             .padding(.top)
             .background(Color(.secondarySystemBackground))
             .navigationTitle("활동 등록")
@@ -98,39 +55,8 @@ struct ActivityCreateView: View {
                     }
                 }
             }
-        }
-    }
-    
-    private func header(title: String, subtitle: String?) -> some View {
-        HStack {
-            Text(title)
-                .sectionHeaderAppearance()
-            if let subtitle {
-                Text(subtitle)
-                    .font(.caption)
             }
         }
-        .setListRowInsets()
-    }
-    
-}
-
-fileprivate extension Text {
-    
-    func sectionHeaderAppearance() -> some View {
-        self
-            .foregroundStyle(.black)
-            .font(.callout)
-            .fontWeight(.regular)
-            .setListRowInsets()
-    }
-    
-}
-
-fileprivate extension View {
-    
-    func setListRowInsets() -> some View {
-        self.listRowInsets(.init(top: 15, leading: 8, bottom: 8, trailing: 15))
     }
     
 }
