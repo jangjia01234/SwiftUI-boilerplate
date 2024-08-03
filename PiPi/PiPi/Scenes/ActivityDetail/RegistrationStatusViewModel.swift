6019ee0b7a33979f7425c0787e84ee257db88d3b
Bell <quf417@gmail.com>
2024-08-03 23:56:54 +0900

    Feat: 최대인원 > 참가인원인 경우 “모집중” , 최대인원이 꽉찬경우 “모집완료” 로 표기함.

---
+++ PiPi/PiPi/Scenes/ActivityDetail/RegistrationStatusViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationStatusViewModel.swift
+//  PiPi
+//
+//  Created by Byeol Kim on 8/3/24.
+//
+
+import SwiftUI
+
+struct RegistrationStatusViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationStatusViewModel()
+}
