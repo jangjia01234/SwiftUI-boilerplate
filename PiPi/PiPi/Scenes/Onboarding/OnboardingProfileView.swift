354e4f452207b406931ee8faf248496193d4b557
hyeyeonie <shy020916@naver.com>
2024-08-05 21:19:20 +0900

    Add: 코드리뷰 수정 - frame 지정한 컴포넌트 수정 - Affiliation enum 생성 - level String -> Int로 변경

--- PiPi/PiPi/Scenes/Onboarding/OnboardingProfileView.swift
+++ PiPi/PiPi/Scenes/Onboarding/OnboardingProfileView.swift
@@ -13,12 +13,11 @@ struct OnboardingProfileView: View {
     @State private var isNavigationActive: Bool = false
     @State private var nickname: String = ""
     @State private var affiliation: String = ""
-    @State private var address: String = ""
+    @State private var email: String = ""
+    @State private var level: Int = 1
     
-    let affiliations = [
-        "포항공대",
-        "애플 디벨로퍼 아카데미"
-    ]
+    
+    let affiliations = Affiliation.allCases.map { $0.rawValue }
     
     var body: some View {
         NavigationStack {
@@ -31,51 +30,62 @@ struct OnboardingProfileView: View {
                 
                 VStack(spacing: 8) {
                     Text("닉네임")
+                        .frame(maxWidth: .infinity, alignment: .leading)
                         .font(.subheadline)
                         .bold()
-                        .padding(.trailing, 260)
+                        .padding(.leading, 8)
                     
-                    TextField("닉네임을 입력해주세요.", text: $nickname)
-                        .textFieldStyle(PlainTextFieldStyle())
-                        .frame(width: 280, height: 32)
-                        .padding(.horizontal, 10)
-                        .background(Color(UIColor.systemGray6))
-                        .cornerRadius(8)
-                        .font(.system(size: 16))
+                    HStack {
+                        TextField("닉네임을 입력해주세요.", text: $nickname)
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .padding(EdgeInsets(top: 4, leading: 10, bottom: 4, trailing: 10))
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(8)
+                            .font(.system(size: 16))
+                            .padding(.horizontal, 10)
+                    }
+
+                    HStack(spacing:0) {
+                        Text("소속")
+                            .font(.subheadline)
+                            .bold()
+                            .padding(.leading, 8)
+                        Spacer()
+                    }
                     
-                    Text("소속")
-                        .font(.subheadline)
-                        .bold()
-                        .padding(.trailing, 270)
-                    
+                    //수정
                     Picker("소속을 선택해주세요.", selection: $affiliation) {
                         ForEach(affiliations, id: \.self) { affiliation in
                             Text(affiliation).tag(affiliation)
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
-                    .frame(width: 300)
-                    .padding(.horizontal, 20)
+                    .padding(.horizontal, 10)
                     
-                    Text("이메일")
-                        .font(.subheadline)
-                        .bold()
-                        .padding(.trailing, 260)
+                    HStack(spacing:0) {
+                        Text("이메일")
+                            .font(.subheadline)
+                            .bold()
+                            .padding(.leading, 8)
+                        Spacer()
+                    }
                     
-                    HStack(spacing: 2) {
-                        TextField("이메일을 입력해주세요.", text: $address)
+                    //이 부분
+                    HStack(spacing: 4) {
+                        TextField("이메일을 입력해주세요.", text: $email)
                             .textFieldStyle(PlainTextFieldStyle())
-                            .frame(width: 150, height: 32)
-                            .padding(.horizontal, 10)
+                            .padding(EdgeInsets(top: 4, leading: 10, bottom: 4, trailing: 10))
                             .autocapitalization(.none) // 첫 대문자 비활성화
                             .background(Color(UIColor.systemGray6))
                             .cornerRadius(8)
                             .font(.system(size: 16))
+                            .padding(.leading, 10)
+                            .padding(.trailing, 4)
                         
                         Text("@icloud.com")
                             .font(.system(size: 14))
                     }
-                    .padding(.trailing, 50)
+                    .padding(.trailing, 40)
                 }
                 .padding(.horizontal, 40)
                 .padding(.bottom, 25)
@@ -110,8 +120,8 @@ struct OnboardingProfileView: View {
             id: userID,
             nickname: nickname,
             affiliation: affiliation,
-            address: "\(address)@icloud.com", // 이메일 주소에 @icloud.com 추가
-            level: "Lv.1" // 예시로 레벨을 기본값으로 설정
+            email: "\(email)@icloud.com",
+            level: level// 예시로 레벨을 기본값으로 설정
         )
         
         do {
