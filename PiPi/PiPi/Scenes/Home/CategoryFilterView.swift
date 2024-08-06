1f7c4047f11673889f527ce81814327c1df929ff
Sangyun Jeong <jsangyun1039@naver.com>
2024-08-06 20:08:39 +0900

    Del: 카테고리 버튼 뷰 삭제
    - CategoryButton 삭제
    - CategoryFilterView에서 각 카테고리 버튼 생성

--- PiPi/PiPi/Scenes/Home/CategoryFilterView.swift
+++ PiPi/PiPi/Scenes/Home/CategoryFilterView.swift
@@ -12,17 +12,27 @@ struct CategoryFilterView: View {
     var body: some View {
         HStack(spacing: 5) {
             ForEach(Activity.Category.allCases, id: \.rawValue) { category in
-                CategoryButton(category: category)
-                    .frame(maxWidth: .infinity)
-                    .background(.white)
-                    .clipShape(
-                        RoundedRectangle(cornerRadius: 100)
-                    )
-                    .setShadow()
+                categoryButton(category)
             }
         }
         .padding(.vertical)
     }
+    
+    private func categoryButton(_ category: Activity.Category) -> some View {
+        HStack {
+            Image(systemName: category.imageName)
+                .resizable()
+                .frame(width: 11, height: 11)
+                .foregroundStyle(.accent)
+                .bold()
+            
+            Text(category.rawValue)
+                .foregroundStyle(.black)
+        }
+        .frame(height: 30)
+        .font(.footnote)
+    }
+    
 }
 
 #Preview {
