06f2a1aa4aefd4ec75e2d4a1c1af8eae5d0ae02c
Sangyun Jeong <jsangyun1039@naver.com>
2024-08-07 02:52:44 +0900

    Add: Constants 추가
    - 포스텍 중심 좌표 상수 추가
    - 카메라 반경 지역 상수 추가

---
+++ PiPi/PiPi/Extensions/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  PiPi
+//
+//  Created by 정상윤 on 8/7/24.
+//
+
+import MapKit
+
+extension CLLocationCoordinate2D {
+    
+    static let postech = CLLocationCoordinate2D(
+        latitude: 36.015501,
+        longitude: 129.322548
+    )
+    
+}
+
+extension MKCoordinateRegion {
+    
+    static let cameraBoundary = MKCoordinateRegion(
+        center: .postech,
+        latitudinalMeters: 3000,
+        longitudinalMeters: 2000
+    )
+    
+}
