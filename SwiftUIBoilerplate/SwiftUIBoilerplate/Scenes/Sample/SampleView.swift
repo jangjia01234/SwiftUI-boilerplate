56fbf7a86e07c5f5481359093d3704fb35afc77e
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-08-08 22:27:10 +0900

    first commit

---
+++ SwiftUIBoilerplate/SwiftUIBoilerplate/Scenes/Sample/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  SwiftUIBoilerplate
+//
+//  Created by Jia Jang on 8/8/24.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SampleView()
+}
